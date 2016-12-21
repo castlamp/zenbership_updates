245dd143343ea2abbb2af45bc0b100e637f4d0c4
Jon Belelieu <jonbelelieu1@Jons-MacBook-Pro-2.local>
2016-12-21 12:06:12 -0500

    v108 and v109 updates

--- 107.php
+++ 107.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Zenbership Update
+ *
+ * @date 		n/a
+ * @link 		http://documentation.zenbership.com/Home/Updating-Zenbership
+ * @version 	v107
+ *
+ * NEW FEATURES
+ * -
+ *
+ * BUG CORRECTIONS
+ * -
+ *
+ * NEW PLUGINS
+ * -
+ */ 
+
 return array(
 	"INSERT INTO `ppSD_templates_email` (`template`, `title`, `desc`, `subject`, `to`, `from`, `cc`, `bcc`, `content`, `format`, `status`, `save`, `track`, `track_links`, `caller_tags`, `custom`, `owner`, `public`, `created`, `header_id`, `footer_id`, `static`, `default_for`, `theme`, `type`) VALUES ('cart_subscription_advanced_notice', 'Subscription: Upcoming Renewal Notice', '', 'Your subscription will renew in %days_until_renewal% days', '', '', '', '', '', 1, 1, 1, 1, 0, '', 0, 2, 0, '0000-00-00 00:00:00', '', '', 1, 0, 'threefiveten', 'template');",
 	"ALTER TABLE `ppSD_cart_sessions` ADD `zip` VARCHAR(8) NOT NULL AFTER `country`;",
@@ -11,4 +28,6 @@ return array(
 	"INSERT INTO `ppSD_templates_email` (`template`, `title`, `desc`, `subject`, `to`, `from`, `cc`, `bcc`, `content`, `format`, `status`, `save`, `track`, `track_links`, `caller_tags`, `custom`, `owner`, `public`, `created`, `header_id`, `footer_id`, `static`, `default_for`, `theme`, `type`) VALUES ('event_canceled',  'Event Canceled',  '',  'Event \"%event:name%\" Has Been Canceled',  '',  '',  '',  '',  '',  '1',  '1',  '0',  '0',  '0',  '',  '',  '2',  '0',  '" . $date . "',  '',  '',  '1',  '',  'threefiveten',  'template');",
 	"UPDATE ppSD_data_eav SET `value`='id,name,tagline,starts,ends,start_registrations,early_bird_end,close_registration,max_rsvps,total_rsvps,allow_guests,max_guests,online,url,location_name,address_line_1,address_line_2,city,state,zip,country,phone' WHERE `key`='event_headings;",
 	"INSERT INTO `ppSD_error_codes` (`code`, `msg`) VALUES ('C019', 'The event you are trying to access has been canceled.');",
+	"ALTER TABLE `ppSD_subscriptions` ADD `next_renew_keep` DATETIME;",
+	"ALTER TABLE `ppSD_products` ADD `auto_register` TINYINT(1);",
 );
---
+++ 109.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Zenbership Update
+ *
+ * @date 		
+ * @link 		http://documentation.zenbership.com/Home/Updating-Zenbership
+ * @version 	v109
+ *
+ * CHANGES
+ * - Pinned notes on homepage and/or or member page.
+ * - Added customizable pipeline (contact) types.
+ *
+ * NEW PLUGINS
+ * - 
+ *
+ * NEW TEMPLATES
+ * - html/donate_campaign_entry.php
+ * - html/donate_campaigns.php
+ * - html/donate_product_entry.php
+ * - html/donate.php
+ * - email/donation_thankyou.html
+ *
+ */ 
+
+return array(
+	"UPDATE ppSD_activity_methods SET `text`='Next action date extended for %act%.' WHERE `id`='extended_next_action' LIMIT 1;",
+	"ALTER TABLE ppSD_notes ADD COLUMN `pin` TINYINT(1) DEFAULT '0';",
+	"ALTER TABLE  `ppSD_notes` ADD COLUMN  `advance_pipeline` TINYINT(1) DEFAULT '0';",
+	"CREATE TABLE `ppSD_pipeline` (`id` int(11) unsigned NOT NULL AUTO_INCREMENT, `name` varchar(50) DEFAULT NULL, `position` int(6) DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;",
+	"INSERT INTO `ppSD_pipeline` (`id`,`name`,`position`) VALUES (1, 'Contact', 1);",
+	"INSERT INTO `ppSD_pipeline` (`id`,`name`,`position`) VALUES (2, 'Lead', 2);",
+	"INSERT INTO `ppSD_pipeline` (`id`,`name`,`position`) VALUES (3, 'Opportunity', 3);",
+	"INSERT INTO `ppSD_pipeline` (`id`,`name`,`position`) VALUES (4, 'Customer', 4);",
+	"ALTER TABLE ppSD_contacts CHANGE `type` `type` INT(5) DEFAULT '1';",
+);
