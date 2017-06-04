2c6740161c0c2eca53a02b836dcd966db3d02b5c
Daniel Kurin <dkurin@swiftlet.technology>
2017-06-03 21:22:51 -0400

    Missing commas causing syntax errors.

--- 115.php
+++ 115.php
@@ -20,9 +20,9 @@
  */ 
 
 return array(
-	"ALTER TABLE `ppSD_criteria_cache` ADD `sort` varchar(60) DEFAULT 'last_name';"
-	"ALTER TABLE `ppSD_criteria_cache` ADD `sort_order` varchar(4) DEFAULT 'ASC';"
-	"ALTER TABLE `ppSD_criteria_cache` ADD `display_per_page` mediumint(5) DEFAULT '50';"
+	"ALTER TABLE `ppSD_criteria_cache` ADD `sort` varchar(60) DEFAULT 'last_name';",
+	"ALTER TABLE `ppSD_criteria_cache` ADD `sort_order` varchar(4) DEFAULT 'ASC';",
+	"ALTER TABLE `ppSD_criteria_cache` ADD `display_per_page` mediumint(5) DEFAULT '50';",
 	"ALTER TABLE `ppSD_staff` ADD `middle_name` varchar(50) DEFAULT NULL;",
 	"ALTER TABLE `ppSD_logins` ADD `notes` MEDIUMTEXT DEFAULT NULL;",
 	"ALTER TABLE `ppSD_uploads` CHANGE `name` `name` varchar(150) DEFAULT '';",
